ac441478ae6b5c4bda8bb5af0dacb1adf802683f
joaomarcossb <joaomarcossb@outlook.com>
2022-02-13 15:09:46 -0300

    links alterados

--- main.js
+++ main.js
@@ -2,16 +2,29 @@ const plusList = document.getElementById('plus-list');
 const plusBtn = document.getElementById('plus-btn');
 const emailClip = document.getElementById('email-clip');
 const wappClip = document.getElementById('wapp-clip');
+const links = document.getElementById('link-list');
 
 const dropDown = () => {
-    if (plusList.style.opacity == 0) {
+    if (plusList.style.visibility == 'hidden') {
         plusList.style.visibility = 'visible';
         plusList.style.opacity = 1;
-        plusList.style.bottom = '0px';
+        plusList.style.bottom = '5px';
+        plusBtn.style.borderColor = '#FA9633';
+        plusBtn.style.borderBottomColor = 'rgb(0,0,0,0)';
+        plusBtn.style.borderBottomWidth = '0px';
+        plusBtn.style.borderRadius = '3px 3px 0px 0px';
+        plusBtn.style.marginTop = '2px';
+        links.style.bottom = '0px';
     } else {
         plusList.style.visibility = 'hidden';
         plusList.style.opacity = 0;
         plusList.style.bottom = '70px';
+        plusBtn.style.borderColor = '#fff';
+        plusBtn.style.borderBottomColor = '#fff';
+        plusBtn.style.borderBottomWidth = '2px';
+        plusBtn.style.borderRadius = '3px';
+        plusBtn.style.marginTop = '0px';
+        links.style.bottom = '45px';
     }
 }
 
