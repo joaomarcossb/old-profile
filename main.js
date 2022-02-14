c2c0925dcd8563173769d0d159de83549da5e5d6
joaomarcossb <joaomarcossb@outlook.com>
2022-02-14 00:17:50 -0300

    links e css

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const plusList = document.getElementById('plus-list');
 const plusBtn = document.getElementById('plus-btn');
-const emailClip = document.getElementById('email-clip');
-const wappClip = document.getElementById('wapp-clip');
 const links = document.getElementById('link-list');
 
 const dropDown = () => {
@@ -26,38 +24,4 @@ const dropDown = () => {
         plusBtn.style.marginTop = '0px';
         links.style.bottom = '45px';
     }
-}
-
-const getWhatsApp = () => {
-    wappClip.style.color = 'lightgreen';
-    if (emailClip.style.color == 'lightgreen') emailClip.style.color = '#fff';
-    setTimeout(() => {
-        navigator.clipboard.writeText('71999768309');
-        alert('Whatsapp copiado para área de transferência!');
-    }, 100)
-}
-
-const getEmail = () => {
-    emailClip.style.color = 'lightgreen';
-    if (wappClip.style.color == 'lightgreen') wappClip.style.color = '#fff';
-    setTimeout(() => {
-        navigator.clipboard.writeText('joaomarcossb@outlook.com');
-        alert('E-mail copiado para área de transferência!');
-    }, 100)
-}
-
-const wappIn = () => {
-    if (wappClip.style.color !== 'lightgreen') wappClip.style.color = '#FA9633';
-}
-
-const wappOut = () => {
-    if (wappClip.style.color !== 'lightgreen') wappClip.style.color = '#fff';
-}
-
-const emailIn = () => {
-    if (emailClip.style.color !== 'lightgreen') emailClip.style.color = '#FA9633';
-}
-
-const emailOut = () => {
-    if (emailClip.style.color !== 'lightgreen') emailClip.style.color = '#fff';
 }
